11d5c28a8ac0e3c47f6943a9e921476f4d4e39ce
Lakendria <kendra939@gmail.com>
2021-06-10 19:06:22 -0400

    react app started

---
+++ index.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 9000
+const path = require("path")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build")))
+
+app.use("/api/",(_,res)=>{
+    res.json({data:"The API is serving data!!!"})
+})
+
+app.listen(port,()=>{
+    console.log(`Server is alive on port ${port}`)
+})
+app.use("*",(_,res)=>{
+    res.sendFile(path.join(__dirname,"client/build","index.html"))
+})
+
+console.log(__dirname)
+console.log(__filename)
