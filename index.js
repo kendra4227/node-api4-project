89c9073008b3880362119abc93a09e6b283920a3
Lakendria <kendra939@gmail.com>
2021-06-10 20:22:16 -0400

    fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname,"client/build")))
 
-app.use("/api/",(_,res)=>{
+app.use("/pizza-app-api1.herokuapp.com/",(_,res)=>{
     res.json({data:"The API is serving data!!!"})
 })
 
