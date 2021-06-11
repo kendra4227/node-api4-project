19e5ee88ce46dad4d4393f14d1320c5ae08fc395
Lakendria <kendra939@gmail.com>
2021-06-10 20:57:51 -0400

    fixed data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname,"client/build")))
+app.use(express.static(path.join(__dirname,"pizza-api/build")))
 
 app.use("/api/",(_,res)=>{
     res.json({data:"The API is serving data!!!"})
